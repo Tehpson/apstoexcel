22d5a4dcde1ce4fa5b588c5bc023e68f40eeac89
Jesper Pettersson <jesper@familjen-pettersson.se>
2023-10-12 21:53:13 +0200

    Updatera instalations fil

--- ApsToExcel Installer.ps1
+++ ApsToExcel Installer.ps1
@@ -1,24 +1,19 @@
-﻿# -*- coding: utf-8 -*-
+﻿# Kontrollera om användaren är administratör
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -Force" -Verb RunAs 
-    exit
+    Write-Host "Scriptet kräver administratörsbehörigheter. Kör om som administratör."
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    return
 }
-try {
-    
-    Write-Host "installerar..."
-    $scriptUrl = "https://raw.githubusercontent.com/Tehpson/apstoexcel/main/Main.ps1"
-    $scriptName = "\apsToExcel.ps1"
-    Set-ExecutionPolicy RemoteSigned -Force
-    Invoke-WebRequest -Uri $scriptUrl -OutFile "$env:USERPROFILE\$scriptName"
 
-    $scriptFolder = [System.IO.Path]::GetDirectoryName("$env:USERPROFILE\$scriptName")
-    [System.Environment]::SetEnvironmentVariable("PATH", $scriptFolder + ";" + [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine), [System.EnvironmentVariableTarget]::Machine)
+Invoke-WebRequest -Uri "https://github.com/Tehpson/apstoexcel/releases/download/1.0.1.2/APS2XLSX.exe" -OutFile "APS2XLSX.exe"
 
-    Install-Module -Name ImportExcel -Force
-    Clear-Host
-    Read-Host "Installationen är klar. Tryck på Enter för att stänga..."
+$installFolder = "C:\Program Files\Aps2Excel"
 
-} catch {
-    Write-Host "Fel: $($_.Exception.Message)"
-    Read-Host "Något Gick fel...."
-}
+Install-Module -Name ImportExcel -Force
+Copy-Item -Path "APS2XLSX.exe" -Destination $installFolder
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut("$env:USERPROFILE\Desktop\APS2XLSX.lnk")
+$shortcut.TargetPath = "$installFolder\APS2XLSX.exe"
+$shortcut.Save()
